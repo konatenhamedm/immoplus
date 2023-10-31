180bc2c8ccf030641e68886aebf3c0bcf6eaf8db
katchenin-13 <katchenin03@gamil.com>
2023-10-31 09:34:58 +0100

    locataire integrer

---
+++ .history/src/Form/LocataireType_20231031013803.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Locataire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LocataireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+           Identification
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+
+
+
+        $resolver->setDefaults([
+            'data_class' => Locataire::class,
+            'doc_required' => true,
+            'doc_options' => [],
+            'validation_groups' => [],
+        ]);
+        $resolver->setRequired('doc_options');
+        $resolver->setRequired('doc_required');
+        $resolver->setRequired(['validation_groups']);
+    }
+}
